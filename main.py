b6c4f8d44cc4df3bec401a7fdafefada70681336
alex <govoruhin.alexandr@yandex.ru>
2022-07-13 19:10:02 +0300

    two commit

--- main.py
+++ main.py
@@ -2,3 +2,14 @@
 # this is test program for git
 
 print("Hello, my name is Alex")
+print("I am study git")
+
+name = "Alex"
+
+
+def myfunc(isname):
+    if isname:
+        print(f"Hello {isname}")
+
+
+myfunc(name)
