c09ae2959e2c7861542a67894bb031a2790cfbc2
alex <govoruhin.alexandr@yandex.ru>
2022-08-02 17:25:54 +0300

    add task_1

---
+++ task_1.py
@@ -0,0 +1,21 @@
+#"Alexander Govorukhin"
+
+# 1
+height = input("Введите свой рост: ",)
+print(height)
+
+# 2
+color = input("Введите свой любимый цвет: ",)
+print(color)
+
+# 3
+a = 0
+b = a + 2
+a = b * 4
+b = a / 3.14
+a = b - 8
+print(a)
+
+# 11
+number = 1234567.456
+print(format(number, ",.1f"))
